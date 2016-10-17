c5311b87c5f8004430d478e6680ab1766256cdae
mark <mercercountynj@gmail.com>
2016-10-17 14:57:42 -0400

    styling

--- src/components/contact/contactMe.js
+++ src/components/contact/contactMe.js
@@ -6,8 +6,6 @@ class contactMe extends Component {
             <div>
                 <h2>HELLO</h2>
                 <p>Contact Me</p>
-        
-                <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
             </div>
         );
     }
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -5,75 +5,66 @@ class Home extends Component {
     render() {
         return (
             <div>
-                <header className="intro">
-                    <div className="intro-body">
-                        <div className="container">
-                            <div className="row">
-                                <div className="col-md-8 col-md-offset-2">
-                                    <h1 className="brand-heading">Grayscale</h1>
-                                    <p className="intro-text">A free, responsive, one page Bootstrap theme.
-                                        <br />Created by Start Bootstrap.</p>
-                                    <a href="#about" className="btn btn-circle page-scroll">
-                                        <i className="fa fa-angle-double-down animated"></i>
-                                    </a>
-                                </div>
+                <div id="myCarousel" className="carousel slide" data-ride="carousel">
+                    <ol className="carousel-indicators">
+                        <li data-target="#myCarousel" data-slide-to="0" className="active"></li>
+                        <li data-target="#myCarousel" data-slide-to="1"></li>
+                    </ol>
+
+                    <div className="carousel-inner" role="listbox">
+                        <div className="item active">
+                            <img src="https://placehold.it/1200x400?text=IMAGE" alt="xyz" />
+                            <div className="carousel-caption">
+                                <h3>Sell $</h3>
+                                <p>Money Money.</p>
+                            </div>
+                        </div>
+
+                        <div className="item">
+                            <img src="https://placehold.it/1200x400?text=Another Image Maybe" alt="xyz" />
+                            <div className="carousel-caption">
+                            <h3>More Sell $</h3>
+                            <p>Lorem ipsum...</p>
                             </div>
                         </div>
                     </div>
-                </header>
 
-                <section id="about" className="container content-section text-center">
+                    <a className="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                        <span className="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                        <span className="sr-only">Previous</span>
+                    </a>
+                    <a className="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                        <span className="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                        <span className="sr-only">Next</span>
+                    </a>
+                </div>
+                
+                <div className="container text-center">
+                    <h3>What We Do</h3><br />
                     <div className="row">
-                        <div className="col-lg-8 col-lg-offset-2">
-                            <h2>About Grayscale</h2>
-                            <p>Grayscale is a free Bootstrap 3 theme created by Start Bootstrap. It can be yours right now, simply download the template on <a href="http://startbootstrap.com/template-overviews/grayscale/">the preview page</a>. The theme is open source, and you can use it for any purpose, personal or commercial.</p>
-                            <p>This theme features stock photos by <a href="http://gratisography.com/">Gratisography</a> along with a custom Google Maps skin courtesy of <a href="http://snazzymaps.com/">Snazzy Maps</a>.</p>
-                            <p>Grayscale includes full HTML, CSS, and custom JavaScript files along with LESS files for easy customization.</p>
+                        <div className="col-sm-4">
+                            <img src="https://placehold.it/150x80?text=IMAGE" className="img-responsive" style={{width: "100%"}} alt="xyz" />
+                            <p>Current Project</p>
                         </div>
-                    </div>
-                </section>
-
-                <section id="download" className="content-section text-center">
-                    <div className="download-section">
-                        <div className="container">
-                            <div className="col-lg-8 col-lg-offset-2">
-                                <h2>Download Grayscale</h2>
-                                <p>You can download Grayscale for free on the preview page at Start Bootstrap.</p>
-                                <a href="http://startbootstrap.com/template-overviews/grayscale/" className="btn btn-default btn-lg">Visit Download Page</a>
+                        <div className="col-sm-4">
+                            <img src="https://placehold.it/150x80?text=IMAGE" className="img-responsive" style={{width: "100%"}} alt="xyz" />
+                            <p>Project 2</p>
+                        </div>
+                        <div className="col-sm-4">
+                            <div className="well">
+                                <p>Some text..</p>
+                            </div>
+                            <div className="well">
+                                <p>Some text..</p>
                             </div>
                         </div>
                     </div>
-                </section>
+                </div><br />
 
-                <section id="contact" className="container content-section text-center">
-                    <div className="row">
-                        <div className="col-lg-8 col-lg-offset-2">
-                            <h2>Contact Start Bootstrap</h2>
-                            <p>Feel free to email us to provide some feedback on our templates, give us suggestions for new templates and themes, or to just say hello!</p>
-                            <p><a href="mailto:feedback@startbootstrap.com">feedback@startbootstrap.com</a>
-                            </p>
-                            <ul className="list-inline banner-social-buttons">
-                                <li>
-                                    <a href="https://twitter.com/SBootstrap" className="btn btn-default btn-lg"><i className="fa fa-twitter fa-fw"></i> <span className="network-name">Twitter</span></a>
-                                </li>
-                                <li>
-                                    <a href="https://github.com/IronSummitMedia/startbootstrap" className="btn btn-default btn-lg"><i className="fa fa-github fa-fw"></i> <span className="network-name">Github</span></a>
-                                </li>
-                                <li>
-                                    <a href="https://plus.google.com/+Startbootstrap/posts" className="btn btn-default btn-lg"><i className="fa fa-google-plus fa-fw"></i> <span className="network-name">Google+</span></a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </section>
-
-                <div id="map"></div>
-
-                <footer>
-                    <div className="container text-center">
-                        <p>Copyright &copy; Your Website 2016</p>
-                    </div>
+                <footer className="container-fluid text-center">
+                    <p>Footer Text</p>
                 </footer>
+
             </div>
         );
     }
